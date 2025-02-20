966e3c4c7cf7e934c3bbb678adf82a2dfd2ab971
halberdssb <jwstevenson22@gmail.com>
2025-02-20 12:24:19 -0500

    player, pickup, camera, scene made

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,17 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody _rb;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        _rb = GetComponent<Rigidbody>();
-    }
+    public float MoveSpeed = 10f;
+    public float RotateSpeed = 75f;
+    private float _vInput;
+    private float _hInput;
 
     // Update is called once per frame
     void Update()
     {
-        
+        _vInput = Input.GetAxis("Vertical") * MoveSpeed;
+        _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
+        transform.Translate(Vector3.forward* _vInput * Time.deltaTime);
+        transform.Rotate(Vector3.up* _hInput * Time.deltaTime);
     }
 }
