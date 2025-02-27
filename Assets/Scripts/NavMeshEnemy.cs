abaef6184b50db16f43253b8e908e5d3aea57c77
halberdssb <jwstevenson22@gmail.com>
2025-02-27 11:27:45 -0500

    navmesh agent work finished

--- Assets/Scripts/NavMeshEnemy.cs
+++ Assets/Scripts/NavMeshEnemy.cs
@@ -1,18 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-public class NavMeshEnemy : MonoBehaviour
+using UnityEngine.AI;
+public class Enemy3 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform patrolRoute; // Parent containing waypoints
+    public Transform player; // Player reference
+    public Material normalMaterial;
+    public Material alertedMaterial;
+    private NavMeshAgent agent;
+    public Transform[] locations;
+    private int currentLocation = 0;
+    private bool chasingPlayer = false;
+    private MeshRenderer meshRenderer;
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        InitializePatrolRoute();
+        MoveToNextPatrolLocation();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!chasingPlayer && !agent.pathPending && agent.remainingDistance < 0.2f)
+        {
+            MoveToNextPatrolLocation();
+        }
+    }
+    void MoveToNextPatrolLocation()
+    {
+        if (locations.Length == 0) return;
+        agent.SetDestination(locations[currentLocation].position);
+        currentLocation = (currentLocation + 1) % locations.Length;
+    }
+    void InitializePatrolRoute()
+    {
+        locations = new Transform[patrolRoute.childCount];
+        for (int i = 0; i < patrolRoute.childCount; i++)
+        {
+            locations[i] = patrolRoute.GetChild(i);
+        }
+    }
+    // Detect when player enters enemy's range
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+
+            Debug.Log("Player detected - start chasing!");
+            meshRenderer.material = alertedMaterial;
+            chasingPlayer = true;
+            agent.SetDestination(player.position);
+        }
+    }
+    // Detect when player leaves enemy's range
+    void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Debug.Log("Player out of range - resume patrol.");
+            meshRenderer.material = normalMaterial;
+            chasingPlayer = false;
+            MoveToNextPatrolLocation();
+        }
     }
 }
